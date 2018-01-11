8856cd3ed64eee4a70cd1ab1b7552ff9bd4bea8c
modalCaiofabio <cfr@modalnetworks.com>
2018-01-11 17:14:56 -0200

    Alterações nos controllers, models.

--- app/controllers/Welcome.php
+++ app/controllers/Welcome.php
@@ -9,8 +9,21 @@ class Welcome extends CI_Controller {
 		$this->load->view('welcome_message');
         }
         public function teste(){
+            echo 'ola';
+          echo  $this->load->database();
             
             
+           
+            
+            $query = $this->db->query("SELECT * FROM MODAL.AUTHENTI");
+  
+            foreach ($query->result() as $row)
+            {
+                    echo $row->login;
+                    echo $row->email;
+                    echo $row->passw;
+            }
+
             
         }
         
